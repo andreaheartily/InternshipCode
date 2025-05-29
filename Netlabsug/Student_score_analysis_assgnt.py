5c7507566c65069548b401c236eda0eadab3f2c7
Andrea Heartily <andreaheartily08@gmail.com>
2025-05-29 22:39:38 +0300

    Update on assignment code and comments

--- Netlabsug/Student_score_analysis_assgnt.py
+++ Netlabsug/Student_score_analysis_assgnt.py
@@ -6,16 +6,60 @@ def get_student_data():
         'name' : input("What's your name? "),
         'scores' : [
             float(input("What's your score for test1?")),
-            float(input("What's your score for test1?")),
-            float(input("What's your score for test1?"))
+            #float(input("What's your score for test2?"))
         ]
     }
     return student
 
-student_data=get_student_data
-
+student_data=get_student_data()
 print("\nStudent Data:")
-print(f"Name: {student_data['name']}")
-print(f"Scores: {student_data['scores']}")
+print(student_data)
+
+def calculate_grade(score):
+    if score >= 90:
+        print("A")
+    elif score >= 80:
+        print("B")
+    elif score >= 80:
+        print("C")
+    else:
+        print("F")
+
+def class_statistics(*scores):
+    minimum = min(scores),
+    maximum = max(scores),
+    average = sum(scores)/len(scores)
+    return minimum, maximum, average
+
+for name, score in student_data.items():
+    grade = calculate_grade(score)
+    print(f"{name} has a score of {score} and a grade of {grade}")
+
+def filter(student):
+    score=list(student.value())
+    average= sum(score)/len(score)
+    dict(filter (lambda item:item[1]>average),student.item[1])
+
+def sum_scores(n):
+    if n == 1:
+        return 1
+    else:
+        return n + sum_scores(n-1)
+
+def user_interaction(student):
+    name = str(input("Enter a name: "))
+    
+    if name in student[name]:
+        score = student[score]
+        grade = calculate_grade(score)
+        print(f"{name} has a score of {score} and a grade of {grade}")
+    else:
+        print(f"{name} does not exist.")
+
+
+
+
+
+
 
 
